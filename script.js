6798eaa4b3fb26180710382b7ca0e235242046d6
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2023-09-23 10:49:31 +0000

    x

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-// Get all control icons
-const controlIcons = document.querySelectorAll('.control-icon');
-const controls = document.querySelector(".controls");
-const themeBtn = document.querySelector(".theme-btn");
+  // Get all control icons
+  const controlIcons = document.querySelectorAll('.control-icon');
+  const controls = document.querySelector(".controls");
+  const themeBtn = document.querySelector(".theme-btn");
 
-// Check if the controls should be hidden on small screens
-function checkScreenSize() {
-  if (window.innerWidth <= 768) { // You can adjust this breakpoint as needed
-    controls.classList.add("hidden");
-  } else {
-    controls.classList.remove("hidden");
-  }
-}
-
-// Toggle controls visibility on theme button click
-themeBtn.addEventListener("click", function() {
-  controls.classList.toggle("hidden");
-});
-
-// Handle control icon clicks
-controlIcons.forEach(icon => {
-  icon.addEventListener("click", function() {
-    // Remove the "active-btn" class from all control icons
-    controlIcons.forEach(otherIcon => {
-      otherIcon.parentElement.classList.remove('active-btn');
-    });
-    // Add the "active-btn" class to the clicked icon
-    icon.parentElement.classList.add('active-btn');
+  themeBtn.addEventListener("click", function() {
+    controls.classList.toggle("hidden");
   });
-});
 
-// Check screen size on page load and resize
-window.addEventListener("load", checkScreenSize);
-window.addEventListener("resize", checkScreenSize);
+  controlIcons.forEach(icon => {
+    const currentPath = window.location.pathname;
+    const iconLink = icon.parentElement.getAttribute("href");
+
+    if (currentPath.includes(iconLink)) {
+      controlIcons.forEach(otherIcon => {
+        otherIcon.parentElement.classList.remove('active-btn');
+      });
+      icon.parentElement.classList.add('active-btn');
+    }
+  });
