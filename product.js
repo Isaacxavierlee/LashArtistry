99e02e8504e3f5f29c3240f84398368d4651e31e
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2023-10-01 03:44:25 +0000

    z

---
+++ product.js
@@ -0,0 +1,21 @@
+  // Get all control icons
+  const controlIcons = document.querySelectorAll('.control-icon');
+  const controls = document.querySelector(".controls");
+  const themeBtn = document.querySelector(".theme-btn");
+
+  themeBtn.addEventListener("click", function() {
+    controls.classList.toggle("hidden");
+  });
+
+  controlIcons.forEach(icon => {
+    const currentPath = window.location.pathname;
+    const iconLink = icon.parentElement.getAttribute("href");
+
+    if (currentPath.includes(iconLink)) {
+      controlIcons.forEach(otherIcon => {
+        otherIcon.parentElement.classList.remove('active-btn');
+      });
+      icon.parentElement.classList.add('active-btn');
+    }
+  });
+
